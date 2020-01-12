8330a9273285723e7fa0f74adfa79ae21ed2e841
adityabisoi <adityabisoi1999@gmail.com>
2020-01-12 11:00:15 +0530

    products model added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=15)
+    date = models.DateField(auto_now_add=True)
+    votes = models.IntegerField(default=1)
+    body = models.TextField()
+    url = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+def __str__(self):
+    return self.title
+
+def limit(self):
+    return self.body[:100]
