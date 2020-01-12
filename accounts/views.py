29c693f3eb9818648b57c62d27819f5bed7b22a9
adityabisoi <adityabisoi1999@gmail.com>
2020-01-12 10:24:33 +0530

    login added

--- accounts/views.py
+++ accounts/views.py
@@ -14,12 +14,21 @@ def signup(request):
                 return redirect('home')
         else:
             return render(request,'accounts/signup.html',{'perror':'Passwords do not match'})
-
     else:
         return render(request,'accounts/signup.html')
 
 def login(request):
-    return render(request,'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['uname'], password=request.POST['pass'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request,'accounts/login.html',{'error':'Incorrect username or password'})
+    else:
+        return render(request,'accounts/login.html')
 
 def logout(request):
-    return render(request,'accounts/signup.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
