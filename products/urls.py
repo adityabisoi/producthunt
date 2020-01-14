bcec5a24e850397beedab85b1cf49bc263c626be
adityabisoi <adityabisoi1999@gmail.com>
2020-01-15 00:14:45 +0530

    home page added

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('create',views.create, name='create')
+    path('create',views.create, name='create'),
+    path('<int:products_id>',views.details, name='details'),
+    path('<int:products_id>/upvote',views.upvote, name='upvote')
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Products
 from django.utils import timezone
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Products.objects
+    return render(request, 'products/home.html',{'products':products})
 
 @login_required
 def create(request):
@@ -21,9 +22,22 @@ def create(request):
             products.image = request.FILES['image']
             products.date = timezone.datetime.now()
             products.hunter = request.user
+            products.votes = 1
             products.save()
-            return redirect('home')
+            return redirect('/products/' + str(products.id))
         else:
             return render(request, 'products/create.html', {'error': 'Please fill all fields'})
     else:
         return render(request, 'products/create.html')
+
+def details(request, products_id):
+    product = get_object_or_404(Products, pk = products_id)
+    return render(request, 'products/details.html', {'products':product})
+
+@login_required
+def upvote(request, products_id):
+    if request.method == 'POST':
+        products = get_object_or_404(Products, pk = products_id)
+        products.votes += 1
+        products.save()
+        return redirect('/products/' + str(products.id))
